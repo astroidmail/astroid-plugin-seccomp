ed45289ed0743543715529ba96c9a45dc41ba21e
M. Dietrich <mdt@emdete.de>
2016-08-09 16:09:39 +0200

    implement random UAs and futile Mid

--- seccomp.py
+++ seccomp.py
@@ -8,6 +8,14 @@ from gi.repository import Gtk
 from gi.repository import WebKit
 from gi.repository import Astroid
 from gi.repository import GMime
+from random import choice, randrange
+
+UAs = (
+	'Apple Mail (2.3124)',
+	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:45.0) Gecko/20100101 Thunderbird/45.1.1',
+	'Mutt/1.5.24 (2015-08-30)',
+	'Outlook',
+	)
 
 class SeccompPlugin (GObject.Object, Astroid.Activatable):
 	object = GObject.property (type=GObject.Object)
@@ -18,12 +26,20 @@ class SeccompPlugin (GObject.Object, Astroid.Activatable):
 	def do_deactivate (self):
 		print ('seccomp: deactivated')
 
-	def get_user_agent(self):
-		print ('seccomp: get_user_agent')
-		return 'mutt'
+	def do_get_user_agent(self):
+		ua = choice(UAs)
+		print ('seccomp: do_get_user_agent', ua)
+		return ua
 
-#	def generated_mid(self):
-#		print ('seccomp: generated_mid')
-#		return ''
-#
+	def do_generate_mid(self):
+		mid = '{}-{}-{}@{:x}.{:02x}'.format(
+			randrange(0x10000),
+			randrange(0x10000),
+			randrange(0x10000),
+			randrange(0x10000),
+			randrange(0x100),
+			)
+		print ('seccomp: do_generate_mid', mid)
+		return mid
+
 print ('seccomp: plugin loaded')
